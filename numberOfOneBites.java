055c28e4fd6da88f4313a564276fcd179ea88d40
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-11-24 23:43:25 +0530

    Add files via upload

---
+++ numberOfOneBites.java
@@ -0,0 +1,12 @@
+public class numberOfOneBites {
+    public int hammingWeight(int n) {
+        int ans = 0;
+
+        for (int i = 0; i < 32; ++i)
+            if (((n >> i) & 1) == 1)
+                ++ans;
+
+        return ans;
+    }
+
+}
