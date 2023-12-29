57db3dc924c5e67c6ecaa3b50e134e1e05f9f78e
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2023-12-29 23:49:51 +0530

    Add files via upload

---
+++ maxConsecutiveOnes.java
@@ -0,0 +1,15 @@
+public class maxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int ans = 0;
+        int sum = 0;
+
+        for (final int num : nums)
+            if (num == 1)
+                ans = Math.max(ans, ++sum);
+            else
+                sum = 0;
+
+        return ans;
+
+    }
+}
