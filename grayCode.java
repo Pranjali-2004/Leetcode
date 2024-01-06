299f7426a9ca62580594a11958e897acbbaeb86a
Pranjali-2004 <143104269+Pranjali-2004@users.noreply.github.com>
2024-01-07 00:22:25 +0530

    Add files via upload

---
+++ grayCode.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class grayCode {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ans = new ArrayList<>();
+        ans.add(0);
+
+        for (int i = 0; i < n; ++i)
+            for (int j = ans.size() - 1; j >= 0; --j)
+                ans.add(ans.get(j) | 1 << i);
+
+        return ans;
+    }
+}
